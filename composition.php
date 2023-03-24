687bb6d390a2c267f9afc1c2051f10ad42b65736
levon80999 <levon.babayan2@tumo.org>
2023-03-24 11:37:37 +0400

    Add examples

--- composition.php
+++ composition.php
@@ -1,30 +1,116 @@
 <?php
-abstract class PromoCode {
+
+declare(strict_types=1);
+
+interface CostStrategy
+{
+    public function cost(Lesson $lesson): int;
+    public function chargeType(): string;
+}
+
+class TimedCostStrategy implements CostStrategy
+{
     /**
-     * @var Order
+     * @param Lesson $lesson
+     * @return int
      */
-    protected $order;
-
-    public function __construct(Order $order) {
-        $this->order = $order;
+    public function cost(Lesson $lesson): int
+    {
+        return ($lesson->getDuration() * 5);
     }
 
-    abstract public function getDiscount();
-}
-
-class DiscountPercentPromoCode extends PromoCode {
-    public function getDiscount() {
-        // count percents from order sum
-    }
-}
-
-class CouponPromoCode extends PromoCode {
-    public function getDiscount() {
-        // count sum according to order and the coupon
+    /**
+     * @return string
+     */
+    public function chargeType(): string
+    {
+        return "hourly rate";
     }
 }
-class DiscountFixedSumPromoCode extends PromoCode {
-    public function getDiscount() {
-        // count sum from order
+
+class FixedCostStrategy implements CostStrategy
+{
+    /**
+     * @param Lesson $lesson
+     * @return int
+     */
+    public function cost(Lesson $lesson): int
+    {
+        return 30;
     }
+
+    /**
+     * @return string
+     */
+    public function chargeType(): string
+    {
+        return "fixed rate";
+    }
+}
+
+class Lesson
+{
+    /**
+     * @var int
+     */
+    private int $duration;
+
+    /**
+     * @var CostStrategy
+     */
+    private CostStrategy $costStrategy;
+
+    /**
+     * @param int $duration
+     * @param CostStrategy $strategy
+     */
+    public function __construct(int $duration, CostStrategy $strategy)
+    {
+        $this->duration = $duration;
+        $this->costStrategy = $strategy;
+    }
+
+    /**
+     * @return int
+     */
+    public function cost(): int
+    {
+        return $this->costStrategy->cost($this);
+    }
+
+    /**
+     * @return string
+     */
+    public function chargeType(): string
+    {
+        return $this->costStrategy->chargeType();
+    }
+
+    /**
+     * @return int
+     */
+    public function getDuration(): int
+    {
+        return $this->duration;
+    }
+}
+
+class Lecture extends Lesson
+{
+    // Lecture-specific implementations ...
+}
+
+class Seminar extends Lesson
+{
+    // Seminar-specific implementations ...
+}
+
+
+$lessons[] = new Lecture(4, new TimedCostStrategy());
+$lessons[] = new Lecture(4, new FixedCostStrategy());
+$lessons[] = new Seminar(4, new FixedCostStrategy());
+
+foreach ($lessons as $lesson) {
+    echo "lesson charge {$lesson->cost()}. ";
+    echo "Charge type: {$lesson->chargeType()}\n";
 }
